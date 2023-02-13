e8e7dbd30edf18b70948048a37022fe7efb50cb4
PRIYANKAGOURAV <118428882+PRIYANKAGOURAV@users.noreply.github.com>
2023-02-13 13:58:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ function changeurl()
 		let p2=document.getElementById("year").value;
 		let p3=document.getElementById("url").innerHTML;
 		if(document.getElementById("name").value && p2==""){
-			document.getElementById("url").innerHTML=p3+" "+"name="+p1;
+			document.getElementById("url").innerHTML=document.getElementById("url").innerHTML+" "+"?"+" "+"name="+p1;
 		}
 			else if(document.getElementById("year").value && p1==""){
-		document.getElementById("url").innerHTML=p3+" "+"year="+p2;
+		document.getElementById("url").innerHTML=document.getElementById("url").innerHTML+" "+"?"+" "+"year="+p2;
 		}
 		else{
-			document.getElementById("url").innerHTML=p3+" "+" "+"name="+" "+p1+" "+"year="+p2;
+			document.getElementById("url").innerHTML=document.getElementById("url").innerHTML+" "+"?"+" "+" "+"name="+" "+p1+" "+"year="+p2;
 		}
 	}
