1da819b59b2504329db7872288127f9c2b8f44b6
PRIYANKAGOURAV <118428882+PRIYANKAGOURAV@users.noreply.github.com>
2023-02-13 13:53:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ function changeurl()
 	{
 		let p1=document.getElementById("name").value;
 		let p2=document.getElementById("year").value;
+		let p3=document.getElementById("url").innerHTML;
 		if(document.getElementById("name").value && p2==""){
-			document.getElementById("url").innerHTML=" https://localhost:8080/"+" "+"name="+p1;
+			document.getElementById("url").innerHTML=p3+" "+"name="+p1;
 		}
 			else if(document.getElementById("year").value && p1==""){
-		document.getElementById("url").innerHTML=" https://localhost:8080/"+" "+"year="+p2;
+		document.getElementById("url").innerHTML=p3+" "+"year="+p2;
 		}
 		else{
-			document.getElementById("url").innerHTML=" https://localhost:8080/"+" "+" "+"name="+" "+p1+" "+"year="+p2;
+			document.getElementById("url").innerHTML=p3+" "+" "+"name="+" "+p1+" "+"year="+p2;
 		}
 	}
