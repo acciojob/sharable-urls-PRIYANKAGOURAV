ee3068a0518909a06649971a65655bc79e4ae38d
PRIYANKAGOURAV <118428882+PRIYANKAGOURAV@users.noreply.github.com>
2023-02-13 13:46:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // your code here
+function changeurl()
+	{
+		let p1=document.getElementById("name").value;
+		let p2=document.getElementById("year").value;
+		if(document.getElementById("name").value && p2==""){
+			document.getElementById("url").innerHTML=" https://localhost:8080/"+" "+"name="+p1;
+		}
+			else if(document.getElementById("year").value && p1==""){
+		document.getElementById("url").innerHTML=" https://localhost:8080/"+" "+"year="+p2;
+		}
+		else{
+			document.getElementById("url").innerHTML=" https://localhost:8080/"+" "+" "+"name="+" "+p1+" "+"year="+p2;
+		}
+	}
